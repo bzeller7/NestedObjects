25f93a05dacfd0e4bef749ca028a6b1d21505aee
bzeller7 <zeller8964@student.cptc.edu>
2019-01-28 12:18:51 -0800

    Created an Advisor Class

---
+++ NestedObjects/Advisor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+
+    /// <summary>
+    /// Represents and advisor from the adivising dept.
+    /// </summary>
+    class Advisor
+    {
+        public string FullName { get; set; }
+
+        public string Email { get; set; }
+
+
+    }
+}
