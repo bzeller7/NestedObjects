2fd3416b6709a8dfd571279967931c3bb86dbdc5
bzeller7 <zeller8964@student.cptc.edu>
2019-01-28 12:26:13 -0800

    Added a Student Class

---
+++ NestedObjects/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+    class Student
+    {
+        /// <summary>
+        /// the full name of the student
+        /// (First Middle Initial Last)
+        /// </summary>
+        public string Name { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+
+        public Advisor AssignedAdvisor { get; set; }
+    }
+}
